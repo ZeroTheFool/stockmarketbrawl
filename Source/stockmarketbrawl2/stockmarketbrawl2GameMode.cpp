cdaebfbceeadd0371ba8db974c0162ce80ebca67
ZeroTheFool <ZeroTheFool@users.noreply.github.com>
2022-12-04 07:22:32 -0500

    new updates
    -tutorial done-ish
    -two character options, subclass of single character class
    -combat works (dropped shares functional, scroll wheel to change type)
    -main menu has actual background (placeholder)
    -added interfaces to make some things work better, don't remember which things
    
    doesn't work
    -choose colour as well as character (doesn't update on client)
    -vendors don't show starting values for client, after 10 seconds works fine when stocks change (this was not previously an issue)
    -tutorial is too wordy, needs more pictures
    
    next
    -update vendor widgets to not just be 3 buttons floating in space
    -fix grainhouse scoreboard, and make modified grainhouse for final scoring instead of stupid green overlay
    -actually test online with real people
    -options menu for game length, stock change interval, dropped shares value, targetable share minimum
    -replace gazebo BSP with actual 3d object
    -add game timer? or at least a notice that 1 minute remains
    
    probably did some destructive things, but overall it seems to work better than it did before and has more of the functionality i'm after

--- Source/stockmarketbrawl2/stockmarketbrawl2GameMode.cpp
+++ Source/stockmarketbrawl2/stockmarketbrawl2GameMode.cpp
@@ -7,9 +7,6 @@
 Astockmarketbrawl2GameMode::Astockmarketbrawl2GameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+
+
 }
